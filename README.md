3b8f2f99db34e9d59677d8eedef2e9e7dabe081f
ValIlya <valyaev.ilya@gmail.com>
2023-10-14 19:33:54 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # captcha-solver
+
+Model
+https://huggingface.co/microsoft/trocr-small-printed
+https://colab.research.google.com/drive/14MfFkhgPS63RJcP7rpBOK6OII_y34jx_
+
+
+
+Dataset
+https://huggingface.co/datasets/hammer888/captcha-data
+
+
---
+++ bin/server.py
@@ -0,0 +1,29 @@
+import gradio as gr
+
+import torch
+from transformers import VisionEncoderDecoderModel
+from transformers import TrOCRProcessor
+
+model_path = 'models/20231014-15-37'
+
+
+processor = TrOCRProcessor.from_pretrained("microsoft/trocr-small-printed")
+model = VisionEncoderDecoderModel.from_pretrained(model_path)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+def process_image(image):
+    pixel_values = processor(image, return_tensors="pt").pixel_values.to(device)
+    generated_ids = model.generate(pixel_values)
+    return processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+
+demo = gr.Interface(
+    fn=process_image,
+    inputs=gr.inputs.Image(type="pil"),
+    outputs="text",
+    examples=['examples/c3xavu.png'],
+)
+
+demo.launch() # share=True
